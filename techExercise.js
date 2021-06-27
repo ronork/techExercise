2825a9649e4d00d2520dda8f02c18026b01ae689
Rohan Grover <grohan95@gmail.com>
2021-06-27 19:32:08 +0530

    Update techExercise.js

--- techExercise.js
+++ techExercise.js
@@ -1,33 +1,55 @@
 //node version used v14.15.4
 
 module.exports = function sortCategoriesForInsert(inputJson) {
-  const part1 = []; //contains  category  with parent_id null
-  const part2 = []; //conatins  category with parent_id not null
-  const part3 = []; //stores the correct order of categories in part2
+  const part1 = [...inputJson],
+    properJsonOutput = [];
 
-  inputJson.map((val) => {
-    if (val.parent_id === null) part1.push(val);
-    else part2.push(val);
-  });
-
-  part2.map((val) => {
-    const parentId = val.parent_id;
-
-    const inPart1 = part1.some((val) => val.id === parentId);
-
-    if (inPart1) part3.push(val);
-    else {
-      const inPart3 = part3.some((val) => val.id === parentId);
-
-      if (inPart3) part3.push(val);
-      else {
-        const inPart2Index = part2.findIndex((val) => val.id === parentId);
-        if (inPart2Index > 0) part3.push(part2[inPart2Index]);
-
-        part3.push(val);
-      }
+  part1.map((val, index) => {
+    if (val.parent_id === null) {
+      part1[index].parent_id = -1;
     }
   });
 
-  return [...part1, ...part3];
-};
+  part1.push({
+    name: "root",
+    id: -1,
+    parent_id: null,
+  });
+
+  const idMapping = part1.reduce((acc, el, i) => {
+    acc[el.id] = i;
+    return acc;
+  }, {});
+  let root;
+  part1.forEach((el) => {
+    if (el.parent_id === null) {
+      root = el;
+      return;
+    }
+    const parentEl = part1[idMapping[el.parent_id]];
+    parentEl.children = [...(parentEl.children || []), el];
+  });
+
+  const traverse = (callback) => {
+    function goThrough(node) {
+      callback(node);
+      node.children
+        ? node.children.forEach((child) => {
+            goThrough(child);
+          })
+        : null;
+    }
+    goThrough(root);
+  };
+
+  traverse(({ name, id, parent_id }) => {
+    if (name !== "root")
+      properJsonOutput.push({
+        name,
+        id,
+        parent_id: parent_id == -1 ? null : parent_id,
+      });
+  });
+
+  return properJsonOutput;
+}
